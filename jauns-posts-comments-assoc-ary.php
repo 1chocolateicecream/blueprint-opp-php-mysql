6edcb798e10a1ab3f65f4530676a7c4082f5c234
alisa <alisa.zavjalkina@icloud.com>
2025-04-04 09:35:55 +0300

    ...ir redzams plans

---
+++ jauns-posts-comments-assoc-ary.php
@@ -0,0 +1,5 @@
+<!-- 1. Izveidot datubāzes savienojumu, izmantojot PDO
+2. Izpildīt LEFT JOIN vaicājumu, lai iegūtu plakanu masīvu
+3. Pārveidot plakano masīvu uz hierarhisku asociatīvo masīvu
+4. Ģenerēt HTML sarakstu no asociatīvā masīva
+5. Aizvērt PDO savienojumu (nav obligāti, bet ieteicams) -->
